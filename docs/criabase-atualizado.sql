ff9780774aca278a4e661c271c58e8e819d07bfa
Yuri Fontella <yurifc4@gmail.com>
2014-11-19 17:06:21 -0200

    Banco de dados atualizado

--- docs/criabase-atualizado.sql
+++ docs/criabase-atualizado.sql
@@ -54,7 +54,7 @@ CREATE TABLE `departamentos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +63,7 @@ CREATE TABLE `departamentos` (
 
 LOCK TABLES `departamentos` WRITE;
 /*!40000 ALTER TABLE `departamentos` DISABLE KEYS */;
-INSERT INTO `departamentos` VALUES (3,'InformÃ¡tica'),(4,'EletrÃ´nicos'),(5,'Brinquedos');
+INSERT INTO `departamentos` VALUES (3,'InformÃ¡tica'),(4,'EletrÃ´nicos'),(5,'Brinquedos'),(6,'MÃ³veis'),(7,'AutomÃ³veis');
 /*!40000 ALTER TABLE `departamentos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +79,7 @@ CREATE TABLE `imagens` (
   `foto` varchar(45) NOT NULL,
   `id_produto` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,7 +174,7 @@ CREATE TABLE `produtos` (
 
 LOCK TABLES `produtos` WRITE;
 /*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (5,'TelevisÃ£o','Full HD Led 40 Polegadas',149.99,4),(6,'Notebook HP','NÃ£o Ã© dos melhores',1970.50,3),(7,'Hulk','Ãšnica peÃ§a no Brasil subdesenvolvido.',200.00,5);
+INSERT INTO `produtos` VALUES (5,'TelevisÃ£o','Full HD Led 40 Polegadas',149.99,4),(6,'Notebook HP','NÃ£o Ã© dos melhores',1970.00,3),(7,'Hulk','Ãšnica peÃ§a no Brasil subdesenvolvido.',200.00,5),(8,'Ford Fusion','Carro 0 KM',30000.00,7);
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,7 +194,7 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `login_UNIQUE` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -203,7 +203,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (6,'admin','admin','admin','d033e22ae348aeb5660fc2140aec35850c4da997');
+INSERT INTO `usuarios` VALUES (6,'admin','admin','admin','d033e22ae348aeb5660fc2140aec35850c4da997'),(7,'Yuri','yurifc4@gmail.com','yuri','48058e0c99bf7d689ce71c360699a14ce2f99774');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -216,4 +216,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-11-18 16:12:41
+-- Dump completed on 2014-11-19 17:06:15
