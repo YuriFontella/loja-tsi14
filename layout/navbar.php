4b753496fc9d1f8ad31e8de32a1dee6c4b1b1c2e
luisaugustos <luis.bc@hotmail.com>
2014-12-01 01:37:03 -0200

    agora acabou

--- layout/navbar.php
+++ layout/navbar.php
@@ -9,7 +9,7 @@
         </a>
       </div> 
       <ul class="nav navbar-nav navbar-right">
-        <li style="margin-top:40px"><a href="<?php echo URL_BASE ?>index.php?acao=carrinho">Carrinho de compras (<?php echo count($carrinho) ?>)</a></li>
+        <li style="margin-top:40px"><a href="<?php echo URL_BASE ?>index.php?acao=carrinho"><span class="glyphicon glyphicon-shopping-cart">   </span> Carrinho de compras (<?php echo count($carrinho) ?>)</a></li>
       </ul>
     </div>
   </div>
--- layout/produto.php
+++ layout/produto.php
@@ -1,22 +1,5 @@
 <?php require_once("layout/head.php") ?>
-
-<div class="alert alert-success notification"><p>Adicionando o produto...</p></div>
-
-<nav class="navbar navbar-default" role="navigation">
-    <div class="container">
-        <div class="collapse navbar-collapse">
-            <div class="logo pull-left">
-                <a href="<?php echo URL_BASE ?>">
-                    <img src="<?php echo URL_BASE ?>assets/img/logo.png" href="#">
-                </a>
-            </div> 
-            <ul class="nav navbar-nav navbar-right">
-                <li style="margin-top:40px"><a href="<?php echo URL_BASE ?>index.php?acao=carrinho"><span class="glyphicon glyphicon-shopping-cart">   </span> Carrinho de compras (<?php echo count($carrinho) ?>)</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
+<?php require_once("layout/navbar.php") ?>
 <body>
 
     <div class="container">
